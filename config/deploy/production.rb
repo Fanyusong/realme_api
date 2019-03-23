b3f69dc69416570842a92f9b1122bd5194fe0428
Tony Pham <kyo.shenshie@gmail.com>
2019-03-24 00:58:45 +0700

    Config realme database

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server ENV['REALME_PRODUCRION_API_SERVER'], port: 22, roles: [:web, :app, :db], primary: true
+server ENV['REALME_API_PRODUCTION_SERVER'], port: 22, roles: [:web, :app, :db], primary: true
 
 set :puma_threads, [4, 16]
-set :puma_workers, 4
+set :puma_workers, 2
