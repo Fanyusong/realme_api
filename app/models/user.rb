0cfd7f79360ed3c98e1ad3459539d0ef17e0a987
Thanh Huynh <phuthanh19951@gmail.com>
2019-06-01 17:50:03 +0700

    format m-d-y for sharing_day field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates  :phone_number, presence: true, uniqueness: true
   validates_presence_of  :name
+
+  def sharing_day
+    attributes['sharing_day'].strftime("%-m/%-d/%Y")
+  end
 end
