1dee747ffe80205b90ba059e4b92f638c87c4ce2
Thanh Huynh <phuthanh19951@gmail.com>
2019-06-01 18:00:57 +0700

    fix sharing day after formating date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,7 +62,7 @@ class ApplicationController < ActionController::API
   end
 
   def sharing
-    if  @current_user.sharing_day.nil? || @current_user.sharing_day.to_time != Date.today.to_time
+    if  @current_user.sharing_day.nil? || @current_user.sharing_day.to_time != Date.today.strftime("%-m/%-d/%Y").to_time
       @current_user.update!({ sharing_day: Date.today, lives: @current_user.lives + 1 })
     end
 
