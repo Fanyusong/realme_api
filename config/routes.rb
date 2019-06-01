b82096ee3af7fad74fd4de0e658a11f3338b9b8f
Thanh Huynh <phuthanh19951@gmail.com>
2019-06-01 14:53:31 +0700

    implement update live

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'register', to: 'application#register'
   post 'sign_in', to: 'application#sign_in'
-  post 'update_live', to: 'application#update_live'
+  put 'update_live', to: 'application#update_live'
 
   root to: "application#home"
   match '*unmatched', to: 'application#route_not_found', via: :all
