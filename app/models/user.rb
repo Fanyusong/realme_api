06df0087c5c5276dddf9177831c9e278fb6ab96e
fanyusong <kyo.shenshie@gmail.com>
2019-09-14 21:31:08 +0700

    remove validate name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates  :phone_number, presence: true, uniqueness: true
-  validates_presence_of  :name
   has_many :posts
 
   def sharing_day
