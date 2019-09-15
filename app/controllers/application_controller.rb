97ff7c7e4739318da6929f69bdae74043b3cfd73
fanyusong <kyo.shenshie@gmail.com>
2019-09-16 01:11:41 +0700

    change url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -107,7 +107,7 @@ class ApplicationController < ActionController::API
           name: post.name,
           title: post.title,
           content: post.content,
-          avatar: url_for(post.avatar).nil? ? nil : url_for(post.avatar)&.gsub('http', 'https')
+          avatar: url_for(post.avatar)
       }
     end
     render json: {
