d74df096337a83fbb57afd5f73abca3b5a7a493f
fanyusong <kyo.shenshie@gmail.com>
2019-09-16 01:10:27 +0700

    Remove

--- lib/active_storage/service/disk_service/no_protocol_disk_service.rb
+++
@@ -1,8 +0,0 @@
-require "active_storage/service/disk_service"
-module ActiveStorage
-  class Service::NoProtocolDiskService < Service::DiskService
-    def url(key, *options)
-      super(key, *options).gsub /http(s)?:/, ''
-    end
-  end
-end
