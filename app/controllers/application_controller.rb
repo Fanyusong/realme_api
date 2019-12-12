ab556bedefc750eb36ae3278024c7e7e780a2766
Tung Pham <kyo.shenshie@gmail.com>
2019-12-12 19:08:33 +0700

    Update percent for winning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -109,12 +109,12 @@ class ApplicationController < ActionController::API
           message: 'Bạn không còn lượt quay'
       }
     end
-    phone_and_headphone_ids = RewardList.where(reward_type_id: RewardType.where(name: ['realme-phone', 'realme-headphone']).pluck(:id)).pluck(:id)
+    phone_and_headphone_ids = RewardList.where(reward_type_id: RewardType.where(name: ['realme-phone', 'realme-headphone']).pluck(:id)).pluck(:id) # 11
     xu100_ids = RewardList.where(reward_type_id: RewardType.where(name: '100xu').pluck(:id)).limit(20).pluck(:id)
     xu500_ids = RewardList.where(reward_type_id: RewardType.where(name: '500xu').pluck(:id)).limit(20).pluck(:id)
-    xu700_ids = RewardList.where(reward_type_id: RewardType.where(name: '700xu').pluck(:id)).limit(20).pluck(:id)
-    hat_ids = RewardList.where(reward_type_id: RewardType.where(name: 'realme-hat').pluck(:id)).limit(20).pluck(:id)
-    failed_ids = (1000000..1000020).to_a
+    xu700_ids = RewardList.where(reward_type_id: RewardType.where(name: '700xu').pluck(:id)).limit(15).pluck(:id)
+    hat_ids = RewardList.where(reward_type_id: RewardType.where(name: 'realme-hat').pluck(:id)).limit(10).pluck(:id)
+    failed_ids = (1000000...1000025).to_a # 25
     total = [phone_and_headphone_ids, xu100_ids, xu500_ids, xu700_ids, hat_ids, failed_ids].flatten
     total.sample
     random_number = total.sample
